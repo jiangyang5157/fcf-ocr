44584af6a73feec776031a31b227459b5b901d45
checkfree\yjiang <yang.jiang@fiserv.com>
2018-06-28 17:33:31 +1200

    rename

--- app/src/main/java/com/fiserv/fcf/ui/MainActivity.kt
+++ app/src/main/java/com/fiserv/fcf/ui/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.container_activity)
+        setContentView(R.layout.activity_main)
 
         if (savedInstanceState == null) {
             replaceFragmentInActivity(R.id.activity_container, instance<MainFragment>())
